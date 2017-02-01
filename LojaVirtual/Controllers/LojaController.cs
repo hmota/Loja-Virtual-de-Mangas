280f2d6576765a53b6e8948dbdcb1be53a10c775
hmota <hmota@outlook.com>
2017-02-01 15:19:03 -0200

    Adicionado Loja

---
+++ LojaVirtual/Controllers/LojaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja/
+        public string Index()
+        {
+            return "Bem vindo ao indice";
+        }
+
+        // GET: Loja/Categoria?Genero=SciFi
+        public string Categoria(string genero)
+        {
+            var mensagem = HttpUtility.HtmlEncode("Categoria: " + genero);
+            return mensagem;
+        }
+
+        // GET: Loja
+        public string Detalhes()
+        {
+            return "Detalhes";
+        }
+    }
+}
