bd4548d9bfe5f5f4ce583edc909cdab9c050629d
hmota <hmota@outlook.com>
2017-02-03 18:00:51 -0200

    Criado validacao de formulario

--- LojaVirtual/App_Start/BundleConfig.cs
+++ LojaVirtual/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace LojaVirtual
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            
         }
     }
 }
--- LojaVirtual/Models/Manga.cs
+++ LojaVirtual/Models/Manga.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LojaVirtual.Models
 {
     [Table("Mangas")]
+    [Bind(Exclude ="AlbumID")]
     public class Manga
     {
+        [ScaffoldColumn(false)]
         public int MangaId { get; set; }
+        [DisplayName("Genero")]
         public int GeneroId { get; set; }
+        [DisplayName("Autor")]
         public int AutorId { get; set; }
 
+        [Required(ErrorMessage ="Necessario titulo do Manga")]
+        [StringLength(160)]
         public string Titulo { get; set; }
+        [Required(ErrorMessage ="Necessario preço")]
+        [Range(0.01, 100.00, ErrorMessage ="Preço deve estar entre 0,01 a 100")]
         public decimal Preco { get; set; }
+        [DisplayName("Capa Manga URL")]
+        [StringLength(1024)]
         public string CapaMangaUrl { get; set; }
 
         public virtual Genero Genero { get; set; }
